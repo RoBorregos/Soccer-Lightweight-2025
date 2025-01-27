040b95edceecbe25733ea674a15164daf8de3652
Camila Tite <a00841255@tec.mx>
2025-01-27 10:41:33 -0500

    Creacion de funciones

--- Motors.h
+++ Motors.h
@@ -19,6 +19,25 @@ class Motors
 		int pin1_2, int pin2_2, int speed_2,
 		int pin1_3, int pin2_3, int speed_3,
 		int pin1_4, int pin2_4, int speed_4),;
+		void InitializeMotors();
+		void InitializeDriver();
+		void SetSpeed(int pwm, int speed);
+		void SetAllSpeeds(int speed);
+		void StopMotors();
+		void MoveForward();
+		void MoveBackward();
+		void MoveLeft();
+		void MoveRight();
+		void MoveMotor1();
+		void MoveMotor2();
+		void MoveMotor3();
+		void MoveMotor4();
+		void MoveMotors(int degree, int speed);
+		void IndividualMotor(int motor, int speed);
+		void GetAllSpeeds();
+		void MoveOneMotor(int motor, int speed);
+		void MoveMotorsImu(double target_angle, double speed_w, int speed);
+		void MoveMotorsProportional(double target_angle, int setpoint);
 		
 
 };
