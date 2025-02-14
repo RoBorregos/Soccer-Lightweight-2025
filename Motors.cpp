e457e10b5e3aedb25151e879b0d3602c8c3e31dc
Camila Tite <a00841255@tec.mx>
2025-02-13 19:55:42 -0600

    Corrections motors

--- Motors.cpp
+++ Motors.cpp
@@ -1,132 +1,124 @@
-#include "Motors.h"  
+#include "Motors.h"
 #include "Arduino.h"
 #include "constants.h"
 
-Motors::Motors(int speed1, int in1_1, int in2_1, int speed2, uint8_t in1_2, int in2_2, uint8_t speed3, uint8_t in1_3, uint8_t in2_3, uint8_t speed4, uint8_t in1_4, uint8_t in2_4) 
-: motor1(speed1, in1_1, in2_1),
-  motor2(speed2, in1_2, in2_2),
-  motor3(speed3, in1_3, in2_3),
-  motor4(speed4, in1_4, in2_4)
-{};
-void Motors::InitializeMotors(){
+Motors::Motors(int speed1, int in1_1, int in2_1, int speed2, int in1_2, int in2_2, int speed3, int in1_3, int in2_3, int speed4, int in1_4, int in2_4)
+    : motor1(in1_1, in2_1, speed1),
+      motor2(in1_2, in2_2, speed2),
+      motor3(in1_3, in2_3, speed3),
+      motor4(in1_4, in2_4, speed4) {}
+
+void Motors::InitializeMotors() {
     motor1.startMotors();
     motor2.startMotors();
     motor3.startMotors();
     motor4.startMotors();
-};
-void Motors::InitializeDriver(){
+}
+
+void Motors::InitializeDriver() {
     motor1.InitializeDriver();
     motor2.InitializeDriver();
-    motor3;.InitializeDriver();
+    motor3.InitializeDriver();
     motor4.InitializeDriver();
+}
 
-};
-void Motors::SetSpeed(int pwm, int speed){
-    motor1.SetSpeed(pwm,speed);
-    motor2.SetSpeed(pwm,speed);
-    motor3.SetSpeed(pwm,speed);
-    motor4.SetSpeed(pwm,speed);
-};
-void Motors::SetAllSpeeds(int speed){
-    motor1.SetSpeed(MOTOR1_PWM,speed);
-    motor2.SetSpeed(MOTOR1_PWM,speed);
-    motor3.SetSpeed(MOTOR1_PWM,speed);
-    motor4.SetSpeed(MOTOR1_PWM,speed);
-};
-void Motors::GetAllSpeeds(){
+void Motors::SetSpeed(int pwm, int speed) {
+    motor1.SetSpeed(speed);
+    motor2.SetSpeed(speed);
+    motor3.SetSpeed(speed);
+    motor4.SetSpeed(speed);
+}
+
+void Motors::SetAllSpeeds(int speed) {
+    motor1.SetSpeed(speed);
+    motor2.SetSpeed(speed);
+    motor3.SetSpeed(speed);
+    motor4.SetSpeed(speed);
+}
+
+void Motors::GetAllSpeeds() {
     Serial.print("Motor 1: ");
-    Serial.println(motor1.GetSpeed_());
+    Serial.println(motor1.getSpeed_());
     Serial.print("Motor 2: ");
-    Serial.println(motor2.GetSpeed_());
+    Serial.println(motor2.getSpeed_());
     Serial.print("Motor 3: ");
-    Serial.println(motor3.GetSpeed_());
+    Serial.println(motor3.getSpeed_());
     Serial.print("Motor 4: ");
-    Serial.println(motor4.GetSpeed_());
+    Serial.println(motor4.getSpeed_());
+}
 
-};
-void Motors::StopMotors(){
+void Motors::StopMotors() {
     motor1.stop();
     motor2.stop();
     motor3.stop();
     motor4.stop();
-};
-void Motors::MoveForward(){
+}
+
+void Motors::MoveForward() {
     motor1.forward();
     motor2.forward();
     motor3.forward();
     motor4.forward();
+}
 
-};
-void Motors::MoveRight(){
+void Motors::MoveRight() {
     StopMotors();
     motor1.forward();
     motor2.backward();
     motor3.backward();
-    motor4.backward();
-};
-void Motors::MoveLeft(){
+    motor4.forward();
+}
+
+void Motors::MoveLeft() {
     StopMotors();
     motor1.backward();
     motor2.forward();
     motor3.forward();
-    motor4.forward();
+    motor4.backward();
+}
 
-};
-void Motors::MoveBackward(){
+void Motors::MoveBackward() {
     StopMotors();
+    motor1.backward();
     motor2.backward();
-    motor3.forward();
-};
-void Motors::MoveMotor1(){
+    motor3.backward();
+    motor4.backward();
+}
+
+void Motors::MoveMotor1() {
     motor1.forward();
-};
-void Motors::MoveMotor2(){
+}
+
+void Motors::MoveMotor2() {
     motor2.forward();
-};
-void Motors::MoveMotor3(){
+}
+
+void Motors::MoveMotor3() {
     motor3.forward();
-};
-void Motors::MoveMotor4(){
+}
+
+void Motors::MoveMotor4() {
     motor4.forward();
-};
+}
 
-void Motors::MoveMotors(int degree, int speed){
-    float m1= cos(((45+degree)*PI/180));
-    float m2= cos(((135+degree)*PI/180));
-    float m3= cos(((225+degree)*PI/180));
-    float m4= cos(((315+degree)*PI/180));
-    int speedA=abs(int(m1*speed));
-    int speedB=abs(int(m1*speed));
-    int speedC=abs(int(m1*speed));
-    int speedD=abs(int(m1*speed));
+void Motors::MoveMotors(int degree, int speed) {
+    float m1 = cos(((45 + degree) * PI / 180));
+    float m2 = cos(((135 + degree) * PI / 180));
+    float m3 = cos(((225 + degree) * PI / 180));
+    float m4 = cos(((315 + degree) * PI / 180));
 
-    analogWrite(motor1.getSpeed_(), speedA);
-    analogWrite(motor2.getSpeed_(), speedB);
-    analogWrite(motor3.getSpeed_(), speedC);
-    analogWrite(motor4.getSpeed_(), speedD);
+    int speedA = abs(int(m1 * speed));
+    int speedB = abs(int(m2 * speed));
+    int speedC = abs(int(m3 * speed));
+    int speedD = abs(int(m4 * speed));
 
-    if (m1 >=0){
-        motor1.forward();
-    }
-    else{
-        motor1.backward();
-    }
-    if (m2 >=0){
-        motor2.forward();
-    }
-    else{
-        motor2.backward();
-    }
-    if (m3 >=0){
-        motor3.forward();
-    }
-    else{
-        motor3.backward();
-    }
-    if (m4 >=0){
-        motor4.forward();
-    }
-    else{
-        motor4.backward();
-    }
-};
+    motor1.SetSpeed(speedA);
+    motor2.SetSpeed(speedB);
+    motor3.SetSpeed(speedC);
+    motor4.SetSpeed(speedD);
+
+    if (m1 >= 0) motor1.forward(); else motor1.backward();
+    if (m2 >= 0) motor2.forward(); else motor2.backward();
+    if (m3 >= 0) motor3.forward(); else motor3.backward();
+    if (m4 >= 0) motor4.forward(); else motor4.backward();
+}
