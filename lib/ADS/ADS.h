c80b8091552fa98828aa2ac586497df161e1e534
camila_tite <a00841255@tec.mx>
2025-02-22 11:49:55 -0600

    ADS actualizationes

--- lib/ADS/ADS.h
+++ lib/ADS/ADS.h
@@ -2,15 +2,38 @@
 #ifndef ARDUINO_ADS_H
 #define ARDUINO_ADS_H
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_ADS1X15.h>
 
-class ADS  
-{
+class ADS  {
 	private:
+	Adafruit_ADS1115 right;
+	Adafruit_ADS1115 left;
+	Adafruit_ADS1115 back;
+	int umbralLinea=3683;
+	int umbralCampo=4159;
 
 	public:
+	ADS();
+	ADS( Adafruit_ADS1115 _Right, Adafruit_ADS1115 _Left, Adafruit_ADS1115 _Back);
 
-		ADS();
-		~ADS();
+	//Inicializa los sensores de IR
+	bool initializeAdafruit();
+	//Promedio de los valores
+	double getAverageRight();
+	double getAverageLeft();
+	double getAverageBack();
+	//Obtener el valor de los sensores
+	int16_t getIndividualValueRight(int channel);
+	int16_t getIndividualValueLeft(int channel);
+	int16_t getIndividualValueBack(int channel);
+	//Detectar linea
+	bool detectLineRight();
+	bool detectLineLeft();
+	bool detectLineBack();
+	//Ajustar movimiento
+	void moveComplementary( bool moveRight, bool moveLeft, bool moveBack);
 
 };
 #endif
+
