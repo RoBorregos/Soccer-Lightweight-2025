834db2750ceb5f826b2a17e712520329b71bc951
camila_tite <a00841255@tec.mx>
2025-03-20 12:55:40 -0600

    Set stricker demo as main to test it

--- src/main.cpp
+++ src/main.cpp
@@ -1,69 +1,44 @@
+#include "statusMachineStricker.h"
+#include "Arduino.h"
+#include "SingleEMAFilterLib.h"
 #include "motors.h"
-#include <Wire.h>
-#include "BNO.h"
-#include <PID.h>
-#include "IRRing.h"
+#include "ConstantsStricker.h"
 
-/*unsigned long start_millis;
-unsigned long current_millis;
-int setpoint = 0;
-int translation_angle = 0;
-int adjust_angle = 0;
+int atack=1;
+SingleEMAFilter<int>filterAnlogo(0.6);
+Motors motorsRobot(kMotor1Pwm, kMotor1In1, kMotor1In2,
+    kMotor2Pwm, kMotor2In1, kMotor2In2,
+    kMotor3Pwm, kMotor3In1, kMotor3In2,
+    kMotor4Pwm, kMotor4In1, kMotor4In2);
 
-unsigned long motor_start_millis = 0;
-unsigned long motor_photo_correction = 90;
+ConstantsStricker:: stateMachine state;
+ConstantsStricker:: sides atack=ConstantsStricker::sides::yellow;
+void setup();
+    startObjects();
+    delay(1600);
+void loop(){
+    state=ConstantsStricker::searchBall;
+   
+   /* if(stateMachine==ConstantsStricker::stateMachine::line{
+        Serial.println(kReceiveLineAngle);
+        if(ultrasoncio<30){
+            kReceiveLineAngle=0;
+        }
+  */ ///Cuando tenga el código se foto
+  //Check if have posesion of the ball
+  if(state==ConstantsStricker::ballDetected){
+        robotIrRing.UpdateData();
+        state=(detector()>15&&abs(robotIrRing.GetAngle())<20)?ConstantsStricker::scoreGoal:ConstantsStricker::searchBall;
 
-IRRing irring;
-// Definir PID con parámetros (ajústalos según tu caso)
-PID pid(6.0, 0.000, 30, 200);
-
-Bno bno;
-//PID pid(0.6, 0.00735, 45, 200);//
-Motors motors(
-  kMotor1Pwm, kMotor1In1, kMotor1In2,
-  kMotor2Pwm, kMotor2In1, kMotor2In2,
-  kMotor3Pwm, kMotor3In1, kMotor3In2,
-  kMotor4Pwm, kMotor4In1, kMotor4In2
-);
-
-void setup() {
-    Serial.begin(9600);
-    unsigned long currentTime = millis();
-    analogReadResolution(12);
-    //ads.initializeAds();
-    motors.InitializeMotors(); 
-    Bno.InitializeBNO();
-    start_millis = millis();
-    irring.init(&currentTime);
-    irring.setOffset(0.0);
+  }
+  //Search ball
+  if(state==ConstantsStricker::searchBall){
+    Serial.println("search");
+    searchBall();
+  }
+  //Go to the goal with the ball
+  if(state==ConstantsStricker::scoreGoal){
+    Serial.println("gol");
+    updateGoalData();
+  }
 }
-
-void loop() {
-
-    double yaw = bno.GetYaw();    
-    translation_angle = 0;
-    adjust_angle = translation_angle - 90;
-    double speed_w = pid.Calculate(setpoint, yaw);
-    if(speed_w != 0){
-        motors.StopAllMotors();
-        motors.MoveBaseWithImu(0, 0, speed_w);
-    }
-
-    irring.updateData();
-    double angle=irring.getAngle();
-    double newAngle=(angle<0 ? 360+angle:angle);
-    newAngle=360-newAngle;
-    double strength=irring.getStrength();
-    
-    if (newAngle > 45 && newAngle < 315) {
-        motors.MoveBaseWithImu(newAngle,150,0);
-        Serial.println("fuera de rango");
-    }
-    else if (newAngle < 45 || newAngle > 315) {
-        motors.StopAllMotors();
-        Serial.println("dentro de rango");
-    }
-}*/
-
-
-
