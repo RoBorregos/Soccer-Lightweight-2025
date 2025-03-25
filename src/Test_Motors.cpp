38846ca6f462c00bb44b962c4fab09183e58eb37
bahena <a01199088tec.mx>
2025-03-25 12:29:21 -0600

    add test motors file

---
+++ src/Test_Motors.cpp
@@ -0,0 +1,19 @@
+#include "motors.h"
+
+Motors motors(
+    kMotor1Pwm, kMotor1In1, kMotor1In2,
+    kMotor2Pwm, kMotor2In1, kMotor2In2,
+    kMotor3Pwm, kMotor3In1, kMotor3In2);
+int i = 0;
+
+void setup() {
+    Serial.begin(9600);
+    motors.InitializeMotors();
+    Serial.println("Prueba de motores iniciada.");
+}
+
+void loop() {
+    for (i; i < 100; i++) {
+        motors.MoveBaseWithImu(1, 150, 0);
+    }
+}
