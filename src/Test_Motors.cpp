9b1a2ea4eaabbbe93c800e2db22b18f30eafc9a6
camila_tite <a00841255@tec.mx>
2025-04-20 20:54:14 -0500

    Agrega la clase Constants_striker y la implementación de stateMachineStriker para la gestión del estado del robot, incluyendo métodos para buscar la pelota y el objetivo. Actualiza la configuración de inicialización de PixyCam en pruebas.

--- src/Test_Motors.cpp
+++ src/Test_Motors.cpp
@@ -27,7 +27,7 @@ void setup() {
     Serial3.begin(19200);
     irring.init(&currentTime);
     irring.SetOffset(0.0);
-    pixy.Init();
+    pixy.Init(SPI_MODE0);
     motors.InitializeMotors();
     Serial.println("Prueba de motores iniciada.");
 }
--- src/Test_Pixy.cpp
+++ src/Test_Pixy.cpp
@@ -7,7 +7,7 @@ void setup()
 {
     Serial.begin(9600);
     Serial3.begin(19200);
-    pixyCamRobot.Init();
+    pixyCamRobot.Init(SPI_MODE0);
 }
 void loop(){
     pixyCamRobot.updateData();
