3252f6474f0372ab26a0d290703f26797bf72462
camila_tite <a00841255@tec.mx>
2025-04-28 19:07:30 -0600

    Ajustar parámetros del PID en Test_IR para mejorar el control del motor y comentar lógica de parada.

--- src/Test_IR.cpp
+++ src/Test_IR.cpp
@@ -13,7 +13,7 @@ float kBallFollowOffset = 1.1;
 Bno bno;
 int kLineCorrectionTime = 200; // Tiempo de corrección en milisegundos
 IRRing irring;
-PID pid(1.2/kMaxPWM, 0/kMaxPWM, 1.3/kMaxPWM, 100);
+PID pid(0.8/kMaxPWM, 0/kMaxPWM, 0.85/kMaxPWM, 100);
 Motors motors(
     kMotor1Pwm, kMotor1In1, kMotor1In2,
     kMotor2Pwm, kMotor2In1, kMotor2In2,
@@ -34,10 +34,10 @@ void loop(){
     double yaw = bno.GetBNOData();
     double speed_w = pid.Calculate(setpoint, yaw);
     motors.MoveOmnidirectionalBase(ballAngle, 0.5, 0);
-    if (speed_w > 0.1 || speed_w < -0.1) {
-        motors.StopAllMotors();
-        motors.MoveOmnidirectionalBase(0, 0, speed_w);
-     }
+    // if (speed_w > 0.1 || speed_w < -0.1) {
+    //     motors.StopAllMotors();
+    //     motors.MoveOmnidirectionalBase(0, 0, speed_w);
+    //  }
 
     Serial.print("Ball angle: ");
     Serial.print(ballAngle);
