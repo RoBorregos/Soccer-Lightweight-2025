09b702273231e4d1db8e94119dd1c3af7db8d7ff
bahena <a01199088tec.mx>
2025-05-08 01:22:28 -0600

    Cleaned main.cpp codes to work on final competition robot

--- src/demoStriker.cpp
+++ src/demoStriker.cpp
@@ -6,20 +6,19 @@
 #include "constants.h"
 #include "PixyCam.h" 
 #include "Photo.h"
+#include "Ultrasonic.h"
 
+bool isCorrecting = false;
 int setpoint = 0;
 float kBallFollowOffsetBack = 1.0;
 float kBallFollowOffsetSide = 1.0;
 float kBallFollowOffsetFront = 1.0;
-unsigned long currentTime = millis();
-float lastKnownGoalX = 0;
-float lastKnownGoalY = 0;
-int targetSignature = 1;
-bool goalDetected = false;
-bool ballControlled = false;
+uint8_t targetSignature = 2;
 const uint32_t kCommunicationMode = SPI_MODE0; //This mode is used because we are using the SPI communication
-int kLineCorrectionTime = 275;
-double kCorrectionDegreeOffset = 0;
+int kLineCorrectionTime = 375;
+double kCorrectionDegreeOffset = 16;
+unsigned long currentTime = millis();
+
 Photo photo(
     kSignalPin1, kMUXPin1_1, kMUXPin2_1, kMUXPin3_1,
     kSignalPin2, kMUXPin1_2, kMUXPin2_2, kMUXPin3_2,
@@ -27,154 +26,43 @@ Photo photo(
 Bno bno;
 PixyCam pixy;
 IRRing irring;
-PID pid(0.9/kMaxPWM, 0.0/kMaxPWM, 0.0/kMaxPWM, 100);
-Motors motors( // motor pins for striker jamaica
+Ultrasonic ultrasonic(kTrigPin, kEchoPin);
+PID pid_static(0.875/kMaxPWM, 0/kMaxPWM, 0/kMaxPWM, 100); 
+PID pid_w(0.9375/kMaxPWM, 0.01/kMaxPWM, 0.01/kMaxPWM, 100); //0.125 0.1 0.0125 domingo 4 de mayo 21:30
+Motors motors(
     kMotor1Pwm, kMotor1In1, kMotor1In2,
-    kMotor2Pwm, kMotor2In2, kMotor2In1,
+    kMotor2Pwm, kMotor2In1, kMotor2In2,
     kMotor3Pwm, kMotor3In1, kMotor3In2);
 
-uint8_t switchPin = 42; // Pin for the switch to start/stop the motors
+uint8_t switchPin = 42;
+
+unsigned long lastUltrasonicReadTime = 0; // Último tiempo de lectura del ultrasonido
+const unsigned long ultrasonicReadInterval = 50; // Intervalo de lectura en milisegundos
+float distanceY = 0; // Última lectura de distancia Y
+float distanceX = 0; // Última lectura de distancia X
+TargetGoalData targetGoalData = {0, 0, 0, 0, 0, 0}; // Estructura para almacenar los datos del objetivo
+
+int X;
+
+uint8_t kGoalkeeperCorrectionTime = 50;
+float correctionStartTime = 0;
 
 void setup() {
     Serial.begin(9600);
     pixy.Init(kCommunicationMode);
     motors.InitializeMotors(switchPin);
     bno.InitializeBNO();
+    ultrasonic.UltrasonicInit();
     irring.init(&currentTime);
     irring.SetOffset(0.0);
-
 }
+
 void loop() {
-    motors.StartStopMotors(switchPin);
-    //--------------------------- Update data from sensors ---------------------------
-    // currentTime = millis();
+    motors.StartStopMotors(switchPin); // Switch a pin digital 
+    pixy.updateData();
     irring.UpdateData();
-    double rawAngle = irring.GetRawAngle();
-    double ballAngle = irring.GetAngle(kBallFollowOffsetBack, kBallFollowOffsetSide, kBallFollowOffsetFront);
-    double yaw = bno.GetBNOData();
-    double speed_w = pid.Calculate(setpoint, yaw);
-    // pixy.updateData();
-    // int numberObjects = pixy.numBlocks();
-    
-    Serial.print("  Raw angle: ");
-    Serial.print(rawAngle);
-    Serial.print("  Ball angle: ");
-    Serial.println(ballAngle);
-
-    // Serial.print("Number of objects: ");
-    // Serial.print(numberObjects);
-
-    motors.MoveOmnidirectionalBase(ballAngle, 0.35, speed_w, kCorrectionDegreeOffset);
-
-    // PhotoData photoDataFront = photo.CheckPhotosOnField(Side::Front);
-    // if (photoDataFront.is_on_line) {
-    //     motors.MoveOmnidirectionalBase(photoDataFront.correction_degree, 1, speed_w, kCorrectionDegreeOffset);
-    //     delay (kLineCorrectionTime);
-    //     motors.StopAllMotors();
-    // }
-
-
+    uint8_t numberObjects = pixy.numBlocks();
+    float yaw = bno.GetBNOData();
+    float ballAngle = irring.GetAngle(kBallFollowOffsetBack, kBallFollowOffsetSide, kBallFollowOffsetFront);
+    float speed_w = pid_w.Calculate(setpoint, yaw);
 }
-
-// #include "motors.h"
-// #include "BNO.h"
-// #include <Arduino.h>
-// #include "IRRing.h"
-// #include "PID.h"
-// #include "constants.h"
-// #include "PixyCam.h" 
-// #include "Photo.h"
-
-// int setpoint = 0;
-// float kBallFollowOffsetBack = 1.0;
-// float kBallFollowOffsetSide = 1.0;
-// float kBallFollowOffsetFront = 1.0;
-// unsigned long currentTime = millis();
-// float lastKnownGoalX = 0;
-// float lastKnownGoalY = 0;
-// int targetSignature = 1;
-// bool goalDetected = false;
-// bool ballControlled = false;
-// const uint32_t kCommunicationMode = SPI_MODE0; //This mode is used because we are using the SPI communication
-// int kLineCorrectionTime = 100;
-// double kCorrectionDegreeOffset = 16;
-// Photo photo(
-//     kSignalPin1, kMUXPin1_1, kMUXPin2_1, kMUXPin3_1,
-//     kSignalPin2, kMUXPin1_2, kMUXPin2_2, kMUXPin3_2,
-//     kSignalPin3, kMUXPin1_3, kMUXPin2_3, kMUXPin3_3);
-// Bno bno;
-// PixyCam pixy;
-// IRRing irring;
-// PID pid(0.9375/kMaxPWM, 0/kMaxPWM, 0/kMaxPWM, 100);
-// Motors motors(
-//     kMotor1Pwm, kMotor1In1, kMotor1In2,
-//     kMotor2Pwm, kMotor2In1, kMotor2In2,
-//     kMotor3Pwm, kMotor3In1, kMotor3In2);
-
-// void setup() {
-//     Serial.begin(9600);
-//     pixy.Init(kCommunicationMode);
-//     motors.InitializeMotors();
-//     bno.InitializeBNO();
-//     irring.init(&currentTime);
-//     irring.SetOffset(0.0);
-
-// }
-// void loop() {
-//     //--------------------------- Update data from sensors ---------------------------
-//     // currentTime = millis();
-//     irring.UpdateData();
-//     double ballAngle = irring.GetAngle(kBallFollowOffsetBack, kBallFollowOffsetSide, kBallFollowOffsetFront);
-//     double yaw = bno.GetBNOData();
-//     double speed_w = pid.Calculate(setpoint, yaw);
-//     // pixy.updateData();
-//     // int numberObjects = pixy.numBlocks();
-    
-//     Serial.print("  Ball angle: ");
-//     Serial.println(ballAngle);
-//     // Serial.print("Number of objects: ");
-//     // Serial.print(numberObjects);
-
-   // motors.MoveOmnidirectionalBase(ballAngle, 0.45, speed_w, kCorrectionDegreeOffset);
-    //--------------------------- Phototransistors reading and correction ---------------------------
-
-//     // PhotoData photoDataLeft = photo.CheckPhotosOnField(Side::Left);
-//     // PhotoData photoDataRight = photo.CheckPhotosOnField(Side::Right);
-//     // PhotoData photoDataFront = photo.CheckPhotosOnField(Side::Front);
-
-//     // if (photoDataLeft.is_on_line) {
-//     //     motors.MoveOmnidirectionalBase(photoDataLeft.correction_degree, 1, 0);
-//     //     delay (kLineCorrectionTime);
-//     //     motors.StopAllMotors();
-//     // } else if (photoDataRight.is_on_line) {
-//     //     motors.MoveOmnidirectionalBase(photoDataRight.correction_degree, 1, 0);
-//     //     delay (kLineCorrectionTime);
-//     //     motors.StopAllMotors();
-//     // if (photoDataFront.is_on_line) {
-//     //     motors.MoveOmnidirectionalBase(photoDataFront.correction_degree, 1, speed_w, kCorrectionDegreeOffset);
-//     //     delay (kLineCorrectionTime);
-//     //     motors.StopAllMotors();
-//     // }
-
-//     //--------------------------- Pixy with motion ---------------------------
-//     // for (int i = 0; i < numberObjects; i++){
-//     //     int signature = pixy.getSignature();
-//     //     Serial.print("  signature: ");
-//     //     Serial.print(signature);
-//     //     if (signature == targetSignature){
-//     //         int x = pixy.getX(i);
-//     //         // Serial.println("x");
-//     //         // Serial.println(x);
-//     //         int y = pixy.getY(i);
-//     //         // Serial.println("y");
-//     //         // Serial.println(y);
-//     //         float goalAngle = (x-158)*(60.0/316.0)*-1;
-//     //         Serial.print("  angleX: ");
-//     //         Serial.println(goalAngle);
-
-//     //         if (abs(ballAngle) < 5){
-//     //             motors.MoveOmnidirectionalBase(goalAngle, 0.5, 0);
-//     //         }
-//     //     }
-//     // }
-// }
